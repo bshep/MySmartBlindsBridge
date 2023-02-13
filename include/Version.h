0bd59bccb0ff6c348c54f660dd2ae1d9964d2ef3
Bruce Sheplan <git.bshep@xoxy.net>
2023-02-12 20:05:50 -0600

    added CSS for blindsCofig textarea

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.51"
+        #define VERSION "0.1.52"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-02-12 19:55:10.937756"
+        #define BUILD_TIMESTAMP "2023-02-12 19:58:25.213370"
     #endif
     

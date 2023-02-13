4fa58a1c7aa83fb7d2bbdbfe560618ca51bda64e
Bruce Sheplan <git.bshep@xoxy.net>
2023-02-13 09:28:35 -0600

    - bump version

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.25"
+        #define VERSION "0.1.26"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2023-02-13 09:18:53.943554"
+        #define BUILD_TIMESTAMP "2023-02-13 09:28:02.687941"
     #endif
     
